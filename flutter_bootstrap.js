7edb7098c3c2814a73df7a0509196152e8531fcb
zendoc <41936768+zendoclab@users.noreply.github.com>
2024-06-07 23:41:30 +0900

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1579611022"
+    serviceWorkerVersion: "2681449809"
   }
 });
